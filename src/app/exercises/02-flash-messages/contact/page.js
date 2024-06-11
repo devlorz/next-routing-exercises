e7f0103accb30957f7b54c4d6caea6a7aa31d39f
Nutti Saelor <nutti.sae@truedigital.com>
2024-06-11 22:19:29 +0700

    feat: fix exercise 2

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,23 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ToastContext } from "../../../../components/ToastProvider";
 
 function ContactPage() {
+  const router = useRouter();
+  const { createToast } = React.useContext(ToastContext);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    router.push("/exercises/02-flash-messages");
+
+    createToast("Your message has been sent!", "success");
+  }
+
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
